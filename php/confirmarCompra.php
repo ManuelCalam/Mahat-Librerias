cbf4ed31c71c613c04072abe574217019426b9fb
ManuelCalam <jmanuelc369@gmail.com>
2024-05-13 23:28:05 -0600

    Prueba 2

--- php/confirmarCompra.php
+++ php/confirmarCompra.php
@@ -140,8 +140,8 @@ function getCarritoProducts(){
         //Attachments
         $mail->addAttachment($filename, 'Detalles_Pedido.pdf');   //Optional name
     
-        header("location: ../index.php");
-        //echo '<script>window.location.href="../index.php";</script>';
+        //header("location: ../index.php");
+        echo '<script>window.location.href="../index.php";</script>';
 
         
 
